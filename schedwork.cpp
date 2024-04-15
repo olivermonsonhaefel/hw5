3e3d562f1b44aa9224b950ea90fa0f1dc456378b
Oliver Monson-Haefel <monsonha@usc.edu>
2024-04-15 18:53:18 +0000

    HW5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,13 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+		const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		int row, int col, std::vector<int> shiftCount
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +43,41 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+		std::vector<int> shiftCount = {};
+		for(int i = 0; i < avail.size(); i++) {
+			std::vector<Worker_T> test(dailyNeed, -1);
+			sched.push_back(test);
+		}
+		for(int i = 0; i < avail[0].size(); i++) {
+			shiftCount.push_back(0);
+		}
+		return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, shiftCount);
 }
 
+bool scheduleHelper(
+		const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		int row, int col, std::vector<int> shiftCount
+) {
+	if(col >= dailyNeed) {
+		col = 0;
+		row ++;
+	}
+	if(row >= avail.size()) {
+		return true;
+	}
+	for(int i = col > 0 ? sched[row][col-1]+1 : 0; i < avail[row].size(); i++) {
+		if(!avail[row][i]) continue;
+		sched[row][col] = i;
+		shiftCount[i] += 1;
+
+		if(shiftCount[i] <= maxShifts) {
+			if(scheduleHelper(avail, dailyNeed, maxShifts, sched, row, col+1, shiftCount)) return true;
+		}
+		shiftCount[i] -= 1;
+		sched[row][col] = -1;
+	}
+	return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void yellowHelper(std::string in, std::string floating, int i, std::set<std::string>& masterList, int numBlanks);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,50 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+		int numBlanks = 0;
+		for(int i = 0; i < in.size(); i++) {
+			if(in[i] == '-') numBlanks ++;
+		}
+		std::set<std::string> masterList = {};
+		yellowHelper(in, floating, 0, masterList, numBlanks);
+		std::set<std::string> returnList = {};
+		for(std::set<std::string>::iterator i = masterList.begin(); i != masterList.end(); i++) {
+			if(dict.find(*i) != dict.end()) {
+				returnList.insert(*i);
+			}
+		}
+		return returnList;
 }
 
 // Define any helper functions here
+
+//sometimes gets stuck in loop, sometimes doesn't generate all the words
+// for some reason won't plug floating into  spot in cases like -r- a or a-- e or --o-- rt
+void yellowHelper(std::string in, std::string floating, int i, std::set<std::string>& masterList, int numBlanks) {
+	if(i > in.size()) {
+		if(floating.size() == 0) {
+			masterList.insert(in);
+		}
+		return;
+	}
+
+	if(in[i] != '-') {
+		yellowHelper(in, floating, i+1, masterList, numBlanks);
+		return;
+	}
+
+	for(int j = 0; j < floating.size(); j++) {
+		in[i] = floating[j];
+		std::string floatingTemp = floating;
+		floatingTemp.erase(j, 1);
+		yellowHelper(in, floatingTemp, i+1, masterList, numBlanks - 1);
+	}
+
+	if(numBlanks > floating.size()) {
+		for(int j = 0; j < 26; j++) {
+			in[i] = 97+j;
+			yellowHelper(in, floating, i+1, masterList, numBlanks - 1);
+		}
+	}
+
+}
